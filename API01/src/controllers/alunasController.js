5add4a4c1331a50e8af91d1a58ae96a57333e64e
amandabacelli <amanda.sted@gmail.com>
2019-10-09 19:58:07 -0300

    get com objeto

--- API01/src/controllers/alunasController.js
+++ API01/src/controllers/alunasController.js
@@ -11,3 +11,16 @@ exports.getById = (req, res) => {
     console.log (id)
     res.status(200).send(alunas.find(aluna => aluna.id == id))
 }
+
+exports.getBooks = (req,res) => {
+    const id = req.params.id //pegar o id da aluna
+    const aluna = alunas.find(aluna => aluna.id == id)
+    console.log(aluna)
+    const livros = aluna.livros
+    const titulosLivros = livros.map(function(element){
+        return element.titulo
+    })
+   
+    res.status(200).send(titulosLivros)
+
+}
--- API01/src/routes/alunasRoute.js
+++ API01/src/routes/alunasRoute.js
@@ -4,4 +4,6 @@ const controller = require("../controllers/alunasController")
 
 router.get('/', controller.get)
 router.get('/:id', controller.getById) //:id significa que vai receber algo, logo qdo coloca a url no postman, coloca o numero do id que quero
+router.get('/:id/books', controller.getBooks)
+
 module.exports = router
